6390edd922b6feeeb3b25681a8419792ad3f216c
bongmiin <102722487+bongmiin@users.noreply.github.com>
2024-12-13 18:43:50 +0900

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Login from './BongMin/pages/Login';
+import Page1 from './BongMin/pages/Page1';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Page1 />
+  </React.StrictMode>
+);
